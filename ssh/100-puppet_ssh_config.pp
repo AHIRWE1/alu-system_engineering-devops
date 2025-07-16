88c8ee1155090819e9785206c8040be07e0234f8
AHIRWE1 <g.ahirwe@alustudent.com>
2025-07-16 08:50:05 -0700

    Updated

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+file_line { 'Declare identity file':
+  path  => '/home/vagrant/.ssh/config',
+  line  => '    IdentityFile ~/.ssh/school',
+  match => '^ *IdentityFile',
+}
+
+file_line { 'Turn off passwd auth':
+  path  => '/home/vagrant/.ssh/config',
+  line  => '    PasswordAuthentication no',
+  match => '^ *PasswordAuthentication',
+}
+
